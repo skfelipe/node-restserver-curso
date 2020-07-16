b144d458c4e57ab18e85beb8a17fd6377414ed57
skfelipe <pipeminus@gmail.com>
2020-07-16 06:39:42 +0200

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -49,7 +49,34 @@ let verificaAdmin_Role = (req, res, next) => {
 };
 
 
+// =========================================
+//  Verificar Token Img
+// =========================================
+
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no Valido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+
+
+}
+
+
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
